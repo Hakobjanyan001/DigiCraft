32f25435386593515178f6c392b1b066860d754f
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-05-27 17:20:48 +0400

    taza

--- C++/classwork/funcia_gumrum.cpp
+++ C++/classwork/funcia_gumrum.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <string>
 
+int sum( int, int, int );
+
+int main() {
+		std::cout << "greq dzer arjeqnery\n" << std::endl; 
+		int num1;
+		int num2;
+		int num3;
+		std::cin >> num1;
+		std::cin >> num2;
+		std::cin >> num3;
+		std::cout << sum(num1, num2, num3) << std::endl;
+return 0;
+}
+
+
 int sum( int iNum1, int iNum2, int iNum3 ) {
 	int iSum = 0;
 	iSum = iNum1 + iNum2 + iNum3;
 	return iSum;
-}
-
-int main() {
-		std::cout << "greq dzer arjeqnery\n" << std::endl; 
-		int iNum1;
-		int iNum2;
-		int iNum3;
-		std::cin >> iNum1;
-		std::cin >> iNum2;
-		std::cin >> iNum3;
-		std::cout << sum(iNum1, iNum2, iNum3 ) << std::endl;
-return 0;
 }
