11296269ac8d76287490ab039d695cfb9e9f4270
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-06-03 19:57:28 +0400

    Jun 3

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -2,25 +2,7 @@
 #include <string>
 
 // std::cout << sizeof(std::string) << std::endl;	
-int sum(int);
 
 int main() {
-		std::cout << "greq 0-ic bardzr tiv: ";
-		int a = 0;
-		std::cin >> a;
-		if (a < 0){
-				std::cout << "ynterq drakan tiv" << std::endl;
-		} else {
-				std::cout << a <<  tvanshanneri gumary` " << sum(a) << std::endl;
-		}	
 	return 0;
-}
-
-int sum(int i){
-	if ( i == 0) {
-		return 0;
-	}
-	int x = i % 10;
-	int y = i / 10;
-	return x + sum(y);
 }
