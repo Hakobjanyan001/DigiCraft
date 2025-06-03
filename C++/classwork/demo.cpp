5949a7e740fece0ab922bc608d31800dccbab372
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-06-03 19:54:41 +0400

    Jun 3

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -5,32 +5,22 @@
 int sum(int);
 
 int main() {
-	int x = 0;
-	std::cin >> x;
-	sum(x);
+		std::cout << "greq 0-ic bardzr tiv: ";
+		int a = 0;
+		std::cin >> a;
+		if (a < 0){
+				std::cout << "ynterq drakan tiv" << std::endl;
+		} else {
+				std::cout << a <<  tvanshanneri gumary` " << sum(a) << std::endl;
+		}	
 	return 0;
 }
 
-int sum(i){
-	int iSum1 = 0;
-	int iSum2 = 0;
-	int iSum3 = 0;
-	for ( int x = i; x <= 10; x = x - 1) {
-	iSum1++;
-		if ( x > 9 ){
-			for ( int y = i; x <= 100; x - 10) {
-			iSum2++;
-				if ( x > 99 ) {
-					for ( int z = i; x <= 1000; x - 100) {
-					iSum3++;
-					return iSum3;
-					}
-				}
-			return iSum2;
-		}
-	return iSum1;
+int sum(int i){
+	if ( i == 0) {
+		return 0;
 	}
+	int x = i % 10;
+	int y = i / 10;
+	return x + sum(y);
 }
-tasnord = 123 / 10
-tasnord --
-miavor = 12/10
