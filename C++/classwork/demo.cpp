6954a7d5001ec44c0e0ba88f49b4e3357cd36b98
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-06-13 20:54:10 +0400

    C++

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -3,27 +3,5 @@
 
 
 int main() {
-int arr[3][3] = {
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}
-		};
-	for (int i = 0; i < 3; i++) {
-		for (int z = 0; z < 3; z++){
-			std::cout << ((arr + i) + z) << std::endl;
-		}
-	}
-
-return 0;
+ return 0;
 }
-
-
-/*int main() {
-int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	for(int i = 0; i < 10; i++) {
-	std::cout << *arr + i << std::endl;
-	}
-int size = sizeof(arr) / sizeof(arr[0]);
-std::cout << size << std::endl;
-}
-*/
---
+++ C++/classwork/zangvac_heapum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+
+/*
+int main() {
+int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	for(int i = 0; i < 10; i++) {
+	std::cout << *(arr + i) << std::endl;
+	}
+int size = sizeof(arr) / sizeof(arr[0]);
+std::cout << size << std::endl;
+}
+
+
+int main() {
+int arr[3][3] = {
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}
+		};
+	for (int i = 0; i < 3; i++) {
+		for (int z = 0; z < 3; z++){
+			std::cout << ((arr + i) + z) << std::endl;
+		}
+	}
+
+return 0;
+}
+
+
+*/
+
+
+
+int main() {
+int* iArr = new int[5];
+	for(int z = 0; z < 5; z++) {
+		std::cin >> *(iArr + z);
+	}
+
+	std::cout << " " << std::endl;
+
+	for(int i = 0; i < 5; i++) {
+		std::cout << *(iArr + i) << std::endl;;
+	}
+delete[] iArr;
+ return 0;
+}
