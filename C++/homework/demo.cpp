5bdc7d2c4ee00285b11ee88d66cdc95bea4ed6ef
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-06-17 23:32:04 +0400

    new

--- C++/homework/demo.cpp
+++ C++/homework/demo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+/*
 class Person {
 	private:
 	int foo;
@@ -24,4 +25,46 @@ int main() {
 	std::cout << mard.area() << std::endl;
 	std::cout << mard.name << std::endl;
 return 0;
+}
+*/
+
+class Address {
+	public:
+		std::string street;
+		std::string	city;
+		std::string	country;
+};
+
+class Person {
+	public:
+	std::string firstName;
+	std::string lastName;
+	Address homeAddress;
+	Address workAddress;
+	Person ( std::string f, std::string l) {
+			firstName = f;
+			lastName = l;
+	};
+		void print () {
+			std::cout << firstName << " " << lastName << std::endl;
+		}
+};
+
+class Employee : public Person{
+	public:
+		float salary;
+		Employee (std::string f, std::string l, float s) : Person(f, l){
+			salary = s;
+		};
+		void print() {
+			Person::print();
+			std::cout << salary << std::endl;
+		}
+};
+
+int main() {
+	Employee a("Karen", "Hakobjanyan", 150000);
+	a.homeAddress.street = "Shinararneri 22/1";
+	a.print();
+	return 0;
 }
