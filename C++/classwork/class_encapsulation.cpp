bcb4e7e2b391502d6c508f5bbe2f2beb42ccf0aa
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-06-23 16:00:13 +0400

    new

---
+++ C++/classwork/class_encapsulation.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+
+/*
+class Student{
+public:
+	Student();
+	~Student();
+	void setName( std::string naem);
+	std::string getName();
+	void setSurname( std::string surnaem);
+	std::string getSurname();
+	void setAge( int age);
+	int getAge();
+	void printStudentInfo();
+private:
+	std::string m_name;
+	std::string m_surname;
+	int m_age;
+
+};
+Student::Student() {
+m_name = " ";
+m_surname = " ";
+m_age = 0;
+}
+
+
+void Student::setName(std::string name) {
+	m_name = name;
+}
+
+std::string Student::getName() {
+	return m_name;
+}
+
+void Student::setSurname(std::string surname) {
+	m_surname = surname;
+}
+
+std::string Student::getSurname() {
+	return m_surname;
+}
+
+void Student::setAge(int age) {
+	m_age = age;
+}
+
+int Student::getAge() {
+ return m_age;
+}
+
+void Student::printStudentInfo() {
+	std::cout << m_name << "\t" << m_surname << "\t" << m_age << std::endl;
+}
+
+Student::~Student() {
+}
+
+int main() {
+	Student st;
+	st.setName("Karen");
+	st.setSurname("Hakobjanyan");
+	st.setAge(24);
+	st.printStudentInfo();
+ return 0;
+}
+*/
+
+
+
+class Student{
+	public:
+	Student();
+	~Student();
+
+	void setName(std::string n);
+	std::string getName();
+	
+	void setSurname(std::string s);
+	std::string getSurname();
+
+	void setAge(int a);
+	int getAge();
+
+	void printStudentInfo();
+
+	private:
+	std::string m_name;
+	std::string m_surname;
+	int m_age;
+};
+
+Student::Student() {
+	m_name = " ";
+	m_surname = " ";
+	m_age = 0;
+}
+
+void Student::setName(std::string n) {
+	m_name = n;
+}
+
+std::string Student::getName() {
+	return m_name ;
+}
+
+void Student::setSurname(std::string s) {
+	m_surname = s;
+}
+
+std::string Student::getSurname() {
+	return m_surname;
+}
+
+void Student::setAge(int a) {
+	m_age = a;
+}
+
+int Student::getAge() {;
+	return m_age;
+}
+
+void Student::printStudentInfo() {
+	std::cout << m_name << "\n" << m_surname << "\n" <<  m_age << std::endl;
+}
+
+Student::~Student(){
+}
+
+int main() {
+	Student st;
+	st.setName("Karen");
+	st.setSurname("Hakobjanyan");
+	st.setAge(24);
+	st.printStudentInfo();
+	return 0;
+}
--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,65 +1,6 @@
 #include <iostream>
 #include <string>
-class Student{
-public:
-	Student();
-	~Student();
-	void setName( std::string naem);
-	std::string getName();
-	void setSurname( std::string surnaem);
-	std::string getSurname();
-	void setAge( int age);
-	int getAge();
-	void printStudentInfo();
-private:
-	std::string m_name;
-	std::string m_surname;
-	int m_age;
-
-};
-Student::Student() {
-m_name = " ";
-m_surname = " ";
-m_age = 0;
-}
-
-
-void Student::setName(std::string name) {
-	m_name = name;
-}
-
-std::string Student::getName() {
-	return m_name;
-}
-
-void Student::setSurname(std::string surname) {
-	m_surname = surname;
-}
-
-std::string Student::getSurname() {
-	return m_surname;
-}
-
-void Student::setAge(int age) {
-	m_age = age;
-}
-
-int Student::getAge() {
- return m_age;
-}
-
-void Student::printStudentInfo() {
-	std::cout << m_name << "\t" << m_surname << "\t" << m_age << std::endl;
-}
-
-Student::~Student() {
-}
 
 int main() {
-	Student st;
-	st.setName("Karen");
-	st.setSurname("Hakobjanyan");
-	st.setAge(24);
-	st.printStudentInfo();
- return 0;
+	return 0;
 }
