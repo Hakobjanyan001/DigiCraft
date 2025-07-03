de7515f8c18bbe63fbc19f65d8f1f2fa28e58c8f
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-07-04 00:22:52 +0400

    04.06.2025

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,40 +1,6 @@
 #include <iostream>
 #include <string>
 
-int funkcia(const int a) { //Value
-return a;
-}
-
 int main() {
-int num = 5;
-int num = funkcia(&num);
-	return 0;
-}
-
-
-//_______________________________________________________
-
-
-int* funkcia(const int a) { //Pointer
-return &a;
-}
-
-int main() {
-int num = 5;
-int& num  = funkcia(&num);
-	return 0;
-}
-
-//______________________________________________________
-
-
-
-int& funkcia(const int& a) { //Reference
-return a;
-}
-
-int main() {
-int num = 5;
-int& num = funkcia(&num);
 	return 0;
 }
