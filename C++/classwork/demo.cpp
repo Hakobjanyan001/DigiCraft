cfff330428d3156009ad6044783ca5d97693525d
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-07-04 20:50:28 +0400

    & new

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 #include <string>
 
+class Animal{
+public:
+Animal(std::string& name) : 
+	name(name){
+}
+std::string getName(){
+return this->name;
+}
+protected:
+const std::string& name;
+};
+
+class Fish : public Animal{
+public:
+Fish(std::string name, int size) : 
+	Animal(name),
+	size(size){
+	}
+int getSize(){
+return this->size;
+}
+protected:
+int size;
+};
+
+class Shark : public Fish{
+public:
+Shark(std::string name, int size, int weight) :
+	Fish(name, size),
+	weight(weight){
+	}
+int getWeight(){
+return this->weight;
+}
+private:
+int weight;
+};
+
 int main() {
+	std::string name = "Akula";
+	Shark sh(name, 4, 55);
+	std::cout << sh.getName() << " " <<sh.getSize() << " " <<  sh.getWeight()  << std::endl;
 	return 0;
 }
