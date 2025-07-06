7851d1232a7a7d6810bb1f4a334c3b16921d0633
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-07-06 13:43:39 +0400

    Pyhton

--- Python/homework/demo.py
+++ Python/homework/demo.py
@@ -1,1 +1,10 @@
-print("Hello Karen")
+# comment -> for run python3 demo,py
+print("result`",5, sep="", end="")
+print(" new", " \" ", )
+
+
+# sep="" -> argumentneri heravorutyuny kam inch dnen ""-i mej da klini argumentneri mejtexum
+# end="" -> hajord tox vorpesi chancni mius printy ayl sharunaki
+
+num1 = input("greq argument: ")
+print(num1)
