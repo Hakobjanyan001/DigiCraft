81be6dab7585773c6d1e4e872cae4000dcea04c4
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-07-07 20:49:51 +0400

    Python das 1

--- Python/classwork/demo.py
+++ Python/classwork/demo.py
@@ -1,5 +1,6 @@
-num = int(input("grir qanaky Studenti"))
+num = int(input("grir qanaky Studenti: "))
 result = 0
+
 def stugum(gnahatakan):
     if gnahatakan < 50:
       print("fail")
@@ -7,10 +8,11 @@ def stugum(gnahatakan):
       print("pass")
     else:
       print("gnahatakany 0ic 100 mijakayqum")
+
 for i in range(num):
-    anun = input("Studenti anun")
-    gnahatakan = int(input("Studenti gnahatakan"))
+    anun = input("Studenti anun: ")
+    gnahatakan = int(input("Studenti gnahatakan: "))
     result += gnahatakan
     stugum(gnahatakan)
-gumar = result / num
+gumar = result/num
 print(gumar)    
