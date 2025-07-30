28b6b803dd5bc92a7528e9c7ae3a7b5acaadebe5
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-07-30 20:06:39 +0400

    data

--- C++/homework/demo.cpp
+++ C++/homework/demo.cpp
@@ -1,41 +1,7 @@
 #include <iostream>
 #include <string>
-class Animal{
-	
-	public:
-	Animal() {}
-	Animal(std::string name, std::string sound) : name(name), sound(sound) {}
-	~Animal() {}
-	
-	public:
-	std::string getName() {return this->name;}
-
-	std::string getSound() {return this->sound;}
-	
-	public:
-	virtual void printInfo(){
-		std::cout << name << " " << sound << std::endl;
-	}
-
-	protected:
-	const std::string name;
-	const std::string sound;
-};
-
-class Dog : public Animal{
-	public:
-	Dog(std::string name, std::string sound, int weight) : Animal(name, sound), weight(weight) {}
-
-	void printInfo(){
-		std::cout << name << " " << sound << " " << weight << std::endl;
-	}
-	
-	protected:
-	int weight;
-};
+ 
 
 int main() {
-	Animal* an = new Dog("Jeko", "Haf", 75);
-	an->printInfo();
 	return 0;
 }
