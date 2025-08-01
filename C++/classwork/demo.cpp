c97d07cf1ce63a41c88948f7290ea04de63d6a69
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-08-01 20:36:24 +0400

    vector

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 #include <string>
 
+class vector{
+	protected:
+	int capacity;
+	int size;
+	int* arr;
+	public:
+	vector() :  size(0)
+				capacity(0)
+				arr(nullptr)
+	{
+	
+	};
+
+	~vector(){
+		delete[] arr;
+	};
+
+
+	void push_back(int e){
+		for(int i = 0; i <= size; i++){
+			if(size = 0){
+				size += 1;
+				vector[element];
+			}else if(size > i) {
+				vector[element]
+				} else {
+					size * 2;
+					vector[element];
+					}
+				}
+	}
+	
+	int pop_back();
+
+	
+};
+
 int main(){
+vector* v = new vector(size);
+v->push_back(20);
+return 0;
 }
