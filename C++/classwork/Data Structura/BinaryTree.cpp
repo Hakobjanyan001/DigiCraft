6fb69b1c3f65338f9c5de78f2a594c1beb4f505b
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-08-18 20:32:32 +0400

    Binary Tree

--- C++/classwork/Data Structura/BinaryTree.cpp
+++ C++/classwork/Data Structura/BinaryTree.cpp
@@ -2,17 +2,25 @@
 #include <string>
 
 class Tree {
-//Helper Struct
-private:
-	struct Node{
-	public:
-		Node(int val) : value(val), left(nullptr), righet(nullptr) {}
-	int value;
-	Node* left;
-	Node* righet;
-	};
+struct Node;
+
+//Public API
+public:
+	void insert(int val) {
+		m_root = insertInner(m_root, val);
+	}
+
+	void preOrder(){
+		preOrder(m_root);
+	}
+
+	void inOrder(){
+		inOrder(m_root);
+	}
 	
-	Node* m_root;
+	void postOrder(){
+		postOrder(m_root);
+	}
 
 //Helper Functions
 private:
@@ -23,42 +31,53 @@ private:
 		}
 		if(val < node->value ) {
 			node->left = insertInner(node->left, val);
-			return insertInner(node, val);
 		} else {
 			node->righet = insertInner(node->righet, val);
-			return insertInner(node, val);
 		}
 		return node;
 	}
 
-//Public API
-public:
-	void insert(int val) {
-		m_root = insertInner(m_root, val);
+	void preOrder(Node* node) {
+	if( node == nullptr) {
+		return;
+	} 
+		std::cout << node->value << std::endl;
+		preOrder(node->left);
+		preOrder(node->righet);
 	}
-
+	}
+	
 	void inOrder(Node* node) {
-	if(node == nullptr){
-		return ;
-	}
-	inOrder(node->left);
-	std::cout << node->value << std::endl;
-	inOrder(node->righet);
-	}	
-
-
-
-public:
-	Tree() : m_root(nullptr) {}
-	~Tree() {
-	while(Tree) {
-	int tmp = Tree
-	delete Tree->righet;
-	delete Tree->left;
-	Tree = tmp;
+	if( node == nullptr) {
+		return;
+	} 
+		inOrder(node->left);
+		std::cout << node->value << std::endl;
+		inOrder(node->righet);
 	}
 	}
 
+	void postOrder(Node* node) {
+	if( node == nullptr) {
+		return;
+	} 
+		postOrder(node->left);
+		postOrder(node->righet);
+		std::cout << node->value << std::endl;	
+	}
+
+//Helper Struct
+private:
+	struct Node{
+	public:
+		Node(int val) : value(val), left(nullptr), righet(nullptr) {}
+	int value;
+	Node* left;
+	Node* righet;
+	};
+	
+//Member Data
+	Node* m_root;
 
 };
 
@@ -68,7 +87,11 @@ myTree->insert(15);
 myTree->insert(20);
 myTree->insert(2);
 myTree->insert(70);
-myTree->insert(0);
+myTree->insert(6);
+myTree->preOrder();
+std::cout << "" << std::endl;
 myTree->inOrder();
+std::cout << "" << std::endl;
+myTree->postOrder();
 	return 0;
 }
