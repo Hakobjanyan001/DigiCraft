2905ee4b9ff172df90b58789886f5583a3e65e09
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-09-17 20:40:06 +0400

    Design Patterns

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,81 +1,65 @@
 #include <iostream>
 #include <string>
-#include <exception>
 #include <memory>
 
-//unique_ptr ________________________________________________
-/*
-int main() {
-	st::unique _ptr<int> uPtr = std::_unique<int>(100);
-	std::cout << *uPtr << std::endl;
-
-	auto p = std::momve(uPtr);
-	std::cout << *p << std::endl;
-
-	if(uPtr != nullptr) {
-	std::cout << *uPtr << std::endl;
-	}
-
-	return 0;
-}
-
-*/
-
 
 /*
-class Person {
+class Singleton{
+private:
+	static Singleton* obj;
+private:
+	Singleton()  {std::cout << "Ctor" << std::endl;}
+	~Singleton() {}
 public:
-Person() { std::cout << "CTR" << std::endl;}
-~Person() {std::cout << "DCTR" << std::endl;}
-};
-
-int main() {
-	std::unique_ptr<Person> uPer = std::make_unique<Person>();
-	//auto uPer = std::make_unique<Person>();  nuyn verevi toxna aveli karj
-	*uPer;
-	auto p = std::move(uPer);
-	*p;
-	return 0;
-}
-*/
-
-
-//shared_ptr ________________________________________________________
-/*
-class Person {
-public:
-Person() { std::cout << "CTR" << std::endl;}
-~Person() {std::cout << "DCTR" << std::endl;}
-};
-
-int main() {
-	std::shared_ptr<Person> sPer = std::make_shared<Person>();
-	//auto uPer = std::make_unique<Person>();  nuyn verevi toxna aveli karj
-	*sPer;
-	std::cout << sPer.use_count() << std::endl;
-	{	
-		auto p = sPer;
-		*p;
-		std::cout << sPer.use_count() << std::endl;;
+	static Singleton* getInstance() {
+		if( nullptr == obj ) {
+			obj = new Singleton();
+		}	
+		return obj;
 	}
+};
+
+Singleton* Singleton::obj = nullptr;
+
+int main() {
+	Singleton* s1 = Singleton::getInstance();
 	return 0;
 }
 */
 
-//wea_ptr _____________________________________________________
-/*
-class Node{
-	std::shared_ptr<Node> next;
-	std::weak_ptr<Node> prev;
+class Shape {
+public:
+	virtual void area() = 0;
+};
+
+class Circle : public Shape {
+public:
+	void area() override {
+		std::cout << "Circle" << std::endl;
+	}
+};
+
+class Rectangle : public Shape {
+public:
+	void area() override {
+		std::cout << "Rectangle" << std::endl;
+	}
+};
+
+
+class ShapeFactory {
+public:
+	static std::unique_ptr<Shape> createShape(const std::string type) {
+		if( type == "Shrjan") return std::make_unique<Circle>();
+		if( type == "Uxxankyun") return std::make_unique<Rectangle>();
+		return nullptr;
+	}
 };
 
 int main() {
-	auto = std::make_shared<Node>(); 
-	return 0;
-}
-*/
-
-
-int main() {
-	return 0;
+	auto s1 = ShapeFactory::createShape("Shrjan");
+	auto s2 = ShapeFactory::createShape("Uxxankyun");
+	s1->area();
+	s2->area();
+return 0;
 }
