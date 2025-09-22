41a842305271d96873d6e0fbb80e576c1888ac98
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-09-22 20:45:34 +0400

    Header & Source

--- C++/classwork/demo.cpp
+++ C++/classwork/demo.cpp
@@ -1,65 +1,7 @@
 #include <iostream>
 #include <string>
-#include <memory>
-
-
-/*
-class Singleton{
-private:
-	static Singleton* obj;
-private:
-	Singleton()  {std::cout << "Ctor" << std::endl;}
-	~Singleton() {}
-public:
-	static Singleton* getInstance() {
-		if( nullptr == obj ) {
-			obj = new Singleton();
-		}	
-		return obj;
-	}
-};
-
-Singleton* Singleton::obj = nullptr;
 
 int main() {
-	Singleton* s1 = Singleton::getInstance();
-	return 0;
-}
-*/
 
-class Shape {
-public:
-	virtual void area() = 0;
-};
-
-class Circle : public Shape {
-public:
-	void area() override {
-		std::cout << "Circle" << std::endl;
-	}
-};
-
-class Rectangle : public Shape {
-public:
-	void area() override {
-		std::cout << "Rectangle" << std::endl;
-	}
-};
-
-
-class ShapeFactory {
-public:
-	static std::unique_ptr<Shape> createShape(const std::string type) {
-		if( type == "Shrjan") return std::make_unique<Circle>();
-		if( type == "Uxxankyun") return std::make_unique<Rectangle>();
-		return nullptr;
-	}
-};
-
-int main() {
-	auto s1 = ShapeFactory::createShape("Shrjan");
-	auto s2 = ShapeFactory::createShape("Uxxankyun");
-	s1->area();
-	s2->area();
 return 0;
 }
