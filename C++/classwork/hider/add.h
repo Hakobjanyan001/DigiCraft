1aa1c3aee10615b444da5781c9289386c2cbcad8
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-09-23 19:42:19 +0400

    Hider & Source

---
+++ C++/classwork/hider/add.h
@@ -0,0 +1,5 @@
+#ifndef __ADD__H
+#define __ADD__H
+int add(int a, int b); 
+#warning "Include meny ../add.h"
+#endif // _ADD_H
---
+++ C++/classwork/source/add.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../hider/add.h"
+
+int add(int a, int b) {
+	return a + b;
+}
+
