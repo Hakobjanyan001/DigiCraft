1968f49bb3aa285964f5f3c705af79bff258382a
Hakobjanyan001 <karen.hakobjanyan0101@gmail.com>
2025-11-06 13:32:20 +0400

    python

---
+++ Python/homework/x.py
@@ -0,0 +1,36 @@
+"""
+class Animal:
+    def sound(self):
+        print("Animal Sound")
+
+class Dog (Animal):
+    def sound(self):
+        print("Dog Sound")
+
+class Cat (Animal):
+    def sound(self):
+        print("Cat Sound")
+
+animals = [ Animal(), Dog() ]
+    for animal in animals:
+        animal.sound()
+"""
+
+
+class Animal:
+    def sound(self):
+        print("Animal Sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog Sound")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat Sound")
+
+def makesound(animal: Animal):
+    animal.sound()
+    makesound(Animal())
+    makesound(Dog())
+    makesound(Cat())
